dde3ef7dc831ff08e52dd5ea4a4dd06af9bac053
Dominick Triola <dominick@ignovadesign.com>
2017-04-16 23:42:27 -0700

    configure router

---
+++ frontend/components/Root.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { HashRouter, Route } from 'react-router-dom';
+import App from './App.jsx';
+
+const Root = ({ store }) => (
+  <Provider store={ store }>
+    <HashRouter>
+      <Route path="/" component={ App } />
+    </HashRouter>
+  </Provider>
+);
+
+export default Root;
--- frontend/olmio.jsx
+++ frontend/olmio.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/Root.jsx';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to Olmio</h1>, root);
+  ReactDOM.render(<Root store={ store } />, root);
 });
 
 // TODO: for checking the store
-import configureStore from './store/store';
 window.store = configureStore();
