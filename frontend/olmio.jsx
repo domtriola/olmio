791bb4442f200fa2ac5492b392994da5d361a994
Dominick Triola <dominick@ignovadesign.com>
2017-04-16 22:15:25 -0700

    setup auth backend

--- frontend/olmio.jsx
+++ frontend/olmio.jsx
@@ -5,3 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Olmio</h1>, root);
 });
+
+// TODO: test these
+import { signup, login, logout } from './util/session_api_util';
+window.signup = signup;
+window.login = login;
+window.logout = logout;
